fabfa2aba14d5845da60ad1c5b32c1b0bf854894
unknown <hadialsaden@gmail.com>
2024-08-30 18:56:50 +0300

    login-page

---
+++ js/login.js
@@ -0,0 +1,15 @@
+
+function errorThrow() {
+
+    let inputValue = document.getElementById("UserNameInput").value;
+    console.log(inputValue)
+    if(inputValue === ""){
+
+        document.getElementById("errorMassage").style.display = "block";
+
+    }else{
+        document.getElementById("errorMassage").style.display = "block";
+}
+
+
+}
